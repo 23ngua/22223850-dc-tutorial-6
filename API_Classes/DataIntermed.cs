55ce5d921989c65c5a22bdb76c21bd1697981b2f
23ngua <nguyenamanda45@gmail.com>
2026-09-21 10:27:36 +0800

    Add shared API data classes

---
+++ API_Classes/DataIntermed.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace API_Classes
+{
+    // Data transferred between the Data API, Business API and GUI
+    public class DataIntermed
+    {
+        public int bal;
+        public uint acct;
+        public uint pin;
+        public string fname;
+        public string lname;
+        public byte[] profilePicture;
+    }
+}
---
+++ API_Classes/SearchData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API_Classes
+{
+    // Data sent from GUI to the Business API for surname searches
+    public class SearchData
+    {
+        public string searhStr;
+    }
+}
